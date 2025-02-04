1b43d9e7b641275ffe4c2a6b75a43ed42bf06def
srinivasa rao sadineni <sadinenisrinivasarao6@gmail.com>
2025-02-04 10:08:08 -0600

    #

--- backend/server.js
+++ backend/server.js
@@ -93,6 +93,6 @@ app.post("/chat", (req, res) => {
 
 // Server Start
 server.listen(process.env.PORT || 5000, () => {
- 
+ console.log(`Server running on port ${process.env.PORT || 5000}`);
   connectDB();
 });
