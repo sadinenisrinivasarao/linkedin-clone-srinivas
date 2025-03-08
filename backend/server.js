36650570559c2d3581611c8fc5691168b5de6f9b
srinivasa rao sadineni <sadinenisrinivasarao6@gmail.com>
2025-03-08 13:51:59 -0600

    #

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", 
+    origin:process.env.CLIENT_URL, 
     credentials: true,
     methods: ["GET", "POST"],
   },  
@@ -36,7 +36,7 @@ const __dirname = path.resolve();
 
   app.use(
     cors({
-      origin: "http://localhost:5173",
+      origin: process.env.CLIENT_URL,
       credentials: true,
     })
   );
